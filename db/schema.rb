d88f1650c17874f9b58db52d1fbabb49dcbef9fe
goudai <clannad.key.36@gmail.com>
2023-08-04 05:44:27 +0900

    users_controller に show メソッドを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_29_042616) do
+ActiveRecord::Schema.define(version: 2023_07_30_030415) do
+
+  create_table "articles", charset: "utf8mb3", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "account"
