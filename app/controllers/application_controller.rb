f88ae958b9adc366b366a9c23e14caf8faea310c
goudai <clannad.key.36@gmail.com>
2023-08-14 17:42:57 +0900

    Revert "CRUD API の実装"
    This reverts commit 006016be984c4657fc212d58c1ad7a5d8a0236fe.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,2 @@
 class ApplicationController < ActionController::Base
-  # CSRF対策をoffにする
-  protect_from_forgery with: :null_session
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,56 +1,10 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show update destroy ]
-
   def index
+    binding.pry
     @users  = User.all
   end
 
   def show
-    # @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
-
-  def create
-    # インスタンスをmodelから作成
-    # @user = User.new(
-    #   name: params[:name],
-    #   account: params[:account],
-    #   email: params[:email],
-    # )
-
-    @user = User.new(user_params)
-
-    # インスタンスをDBに保存する
-    @user.save!
-
-    # jsonとして値を返す
-    render :show
-
-  end
-
-  def update
-    # 対象のレコードを探す
-    # @user = User.find(params[:id])
-    # 探してきたレコードに対して変更を行う
-    @user.update!(user_params)
-    # jsonとして値を返す
-    render :show
-  end
-
-  def destroy
-    # 対象のレコードを探す
-    # @user = User.find(params[:id])
-    # 探してきたレコードを削除する
-    @user.destroy!
-    # jsonとして値を返す
-    render :show
-  end
-
-  private
-    def user_params
-      params.require(:user).permit(:name, :account, :email)
-    end
-
-    def set_user
-      @user = User.find(params[:id])
-    end
 end
