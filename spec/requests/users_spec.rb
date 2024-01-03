5cb477d96b462f9beb8b62628552b73f3dec7042
goudai <clannad.key.36@gmail.com>
2024-01-03 12:31:41 +0900

    users API の request spec の概要を作成

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+  describe "GET /users" do
+    it "ユーザーの一覧を取得できる" do
+    end
+  end
+
+  describe "GET /users/:id" do
+    it "任意のユーザーレコードを取得できる" do
+    end
+  end
+
+  describe "POST /users" do
+    it "ユーザーのレコードを作成できる" do
+    end
+  end
+
+  describe "PATCH /users/:id" do
+    it "任意のユーザーレコードを更新できる" do
+    end
+  end
+
+  describe "DELETE /users/:id" do
+    it "任意のユーザーレコードを削除できる"
+    end
+  end
+end
